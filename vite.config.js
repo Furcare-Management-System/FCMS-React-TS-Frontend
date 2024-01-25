ac61bde687ba6954d94322108068d63d49a358aa
Dianne Bastillada <diannebastillada87@gmail.com>
2024-01-25 22:31:36 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/furcare-frontend/",
+  base:"/furcare-web/",
   plugins: [react()],
 });
