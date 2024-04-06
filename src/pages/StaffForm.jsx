f5ce4537c27b2ad38bb07e3095c92707b7cb18ce
Dianne Bastillada <diannebastillada87@gmail.com>
2024-04-06 23:02:42 +0800

    auto generate staff account

--- src/pages/StaffForm.jsx
+++ src/pages/StaffForm.jsx
@@ -4,18 +4,13 @@ import axiosClient from "../axios-client";
 import {
   TextField,
   Box,
-  Button,
-  Stepper,
-  Step,
-  StepLabel,
   Typography,
   InputAdornment,
   Paper,
   CircularProgress,
-  FormControlLabel,
-  Checkbox,
 } from "@mui/material";
 import Swal from "sweetalert2";
+import { LoadingButton } from "@mui/lab";
 
 export default function StaffForm() {
   const navigate = useNavigate();
@@ -24,6 +19,7 @@ export default function StaffForm() {
   const [selectedZipcode, setSelectedZipcode] = useState(null);
   const [zipcodeerror, setZipcodeerror] = useState(null);
   const [zipcodeloading, setZipcodeloading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const [staff, setStaff] = useState({
     id: null,
@@ -34,8 +30,6 @@ export default function StaffForm() {
     barangay: "",
     zone: "",
     email: "",
-    password: "",
-    password_confirmation: "",
   });
 
   const [zipcode, setZipcode] = useState({
@@ -45,31 +39,14 @@ export default function StaffForm() {
     zipcode: "",
   });
 
-  const [activeStep, setActiveStep] = useState(0);
-
-  const confirmPassword = (ev) => {
-    ev.preventDefault();
-    if (staff.password === staff.password_confirmation) {
-      setActiveStep((prevStep) => prevStep + 1);
-    } else {
-      // toast.error("Password did not match.", {
-      //   position: "bottom-right",
-      //   theme: "colored",
-      // });
-      Swal.fire({
-        text: "Password did not match!",
-        icon: "error",
-      });
-    }
-  };
-
   const onSubmit = (ev) => {
     ev.preventDefault();
     setErrors(null);
-
+    setLoading(true);
     axiosClient
       .post(`/staffs`, staff)
       .then((response) => {
+        setLoading(false);
         Swal.fire({
           text: "Staff registration has been saved!",
           icon: "success",
@@ -82,6 +59,7 @@ export default function StaffForm() {
       })
       .catch((err) => {
         handleErrors(err);
+        setLoading(false);
       });
   };
 
@@ -89,29 +67,8 @@ export default function StaffForm() {
     const response = err.response;
     if (response && response.status === 422) {
       setErrors(response.data.errors);
-      if (errors.email || errors.password) setActiveStep(0);
     }
   };
-
-  const handleNext = (e) => {
-    e.preventDefault();
-
-    if (activeStep === 0) {
-      confirmPassword(e);
-      return true;
-    }
-    if (activeStep === 1) {
-      onSubmit(e);
-      return true;
-    }
-    setActiveStep((prevStep) => prevStep + 1);
-  };
-
-  const handlePrev = () => {
-    setActiveStep((prevStep) => prevStep - 1);
-  };
-
-  const steps = ["Create a User Account", "Staff Registration"];
 
   useEffect(() => {
     let timerId;
@@ -123,7 +80,7 @@ export default function StaffForm() {
       setZipcode({});
       setZipcodeerror(null);
       getZipcodeDetails(selectedZipcode);
-      setZipcodeloading(false);
+      // setZipcodeloading(false);
     }, 1000);
 
     return () => {
@@ -142,6 +99,7 @@ export default function StaffForm() {
         .then(({ data }) => {
           setZipcode(data.data);
           setStaff((prevStaff) => ({ ...prevStaff, zipcode_id: data.data.id }));
+          setZipcodeloading(false);
         })
         .catch((error) => {
           const response = error.response;
@@ -156,118 +114,35 @@ export default function StaffForm() {
     setSelectedZipcode(event.target.value);
   };
 
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleCheckboxChange = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const getStepContent = (step) => {
-    switch (step) {
-      case 0:
-        return (
-          <Box
-            sx={{
-              width: "80%",
-              display: "flex",
-              alignItems: "center",
-              flexDirection: "column",
-              "& > :not(style)": { m: 1 },
-              margin: "auto",
-            }}
-          >
-            <Typography variant="h5" padding={1}>
-              Create an Account
-            </Typography>
-
-            <TextField
-              id="Email"
-              label="Email"
-              size="small"
-              type="email"
-              value={staff.email}
-              onChange={(ev) => setStaff({ ...staff, email: ev.target.value })}
-              fullWidth
-              required
-              error={errors && errors.email ? true : false}
-              helperText={
-                errors && errors.email
-                  ? errors && errors.email
-                  : "Please input a valid email address."
-              }
-              autoFocus
-            />
+  return (
+    <Paper
+      sx={{
+        width: "70%",
+        margin: "3%",
+        padding: "15px",
+        border: "2px solid black",
+      }}
+    >
+      <form onSubmit={(e) => onSubmit(e)} style={{ alignItems: "center" }}>
+        <Box
+          sx={{
+            width: "90%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            "& > :not(style)": { m: 1 },
+            margin: "auto",
+          }}
+        >
+          <Typography variant="h5" padding={1} align="center">
+            Staff Registration
+          </Typography>
+          <Box display={"flex"} flexDirection={"row"} sx={{ width: "100%" }}>
             <TextField
               variant="outlined"
-              id="Password"
               size="small"
-              label="Password"
-              type={showPassword ? "text" : "password"}
-              required
-              fullWidth
-              value={staff.password}
-              onChange={(ev) =>
-                setStaff({ ...staff, password: ev.target.value })
-              }
-              error={errors && errors.password ? true : false}
-              helperText={
-                errors && errors.password
-                  ? errors && errors.password
-                  : "Your password must be at least 8 characters long and contain numbers and letters."
-              }
-            />
-            <TextField
-              variant="outlined"
-              id="Password Confirmation"
-              label="Password Confirmation"
-              size="small"
-              fullWidth
-              required
-              type={showPassword ? "text" : "password"}
-              value={staff.password_confirmation}
-              onChange={(ev) =>
-                setStaff({
-                  ...staff,
-                  password_confirmation: ev.target.value,
-                })
-              }
-              error={errors && errors.password ? true : false}
-            />
-            <FormControlLabel
-              sx={{ width: "100%" }}
-              control={
-                <Checkbox
-                  checked={showPassword}
-                  onChange={handleCheckboxChange}
-                  color="primary"
-                />
-              }
-              label="Show Password"
-            />
-          </Box>
-        );
-
-      case 1:
-        return (
-          <Box
-            sx={{
-              width: "80%",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              "& > :not(style)": { m: 1 },
-              margin: "auto",
-            }}
-          >
-            <Typography variant="h5" padding={1}>
-              Staff Registration
-            </Typography>
-
-            <TextField
-              variant="outlined"
               id="firstname"
               label="Firstname"
-              size="small"
               value={staff.firstname}
               onChange={(ev) =>
                 setStaff({ ...staff, firstname: ev.target.value })
@@ -279,103 +154,119 @@ export default function StaffForm() {
             />
             <TextField
               variant="outlined"
+              size="small"
               id="Lastname"
               label="Lastname"
-              size="small"
+              fullWidth
               value={staff.lastname}
               onChange={(ev) =>
                 setStaff({ ...staff, lastname: ev.target.value })
               }
-              fullWidth
               required
               error={errors && errors.lastname ? true : false}
               helperText={errors && errors.lastname}
             />
+          </Box>
+          <TextField
+            id="Email"
+            label="Email"
+            size="small"
+            type="email"
+            fullWidth
+            value={staff.email || ""}
+            onChange={(ev) => setStaff({ ...staff, email: ev.target.value })}
+            // required
+            error={errors && errors.email ? true : false}
+            helperText={
+              errors && errors.email
+                ? errors && errors.email
+                : "Please input a valid email address."
+            }
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            size="small"
+            id="Contact Number"
+            label="Contact Number"
+            type="number"
+            fullWidth
+            inputProps={{
+              minLength: 10,
+            }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">+63</InputAdornment>
+              ),
+            }}
+            value={staff.contact_num}
+            onChange={(ev) => {
+              const input = ev.target.value.slice(0, 10);
+              setStaff({ ...staff, contact_num: input });
+            }}
+            required
+            error={errors && errors.contact_num ? true : false}
+            helperText={errors && errors.contact_num}
+          />
+
+          <TextField
+            id="Zone"
+            size="small"
+            label="Zone/Block/Street"
+            fullWidth
+            value={staff.zone || ""}
+            onChange={(ev) => setStaff({ ...staff, zone: ev.target.value })}
+            error={errors && errors.zone ? true : false}
+            helperText={errors && errors.zone}
+          />
+          <TextField
+            id="Barangay"
+            label="Barangay"
+            size="small"
+            fullWidth
+            value={staff.barangay}
+            onChange={(ev) => setStaff({ ...staff, barangay: ev.target.value })}
+            required
+            error={errors && errors.barangay ? true : false}
+            helperText={errors && errors.barangay}
+          />
+
+          <Box display={"flex"} flexDirection={"row"} sx={{ width: "100%" }}>
             <TextField
-              variant="outlined"
-              id="Contact Number"
-              label="Contact Number"
+              id="Zipcode"
+              label="Zipcode"
               size="small"
               type="number"
-              inputProps={{
-                minLength: 10,
-              }}
+              value={selectedZipcode}
+              onChange={handleZipcodeChange}
+              fullWidth={!zipcode.area}
+              required
+              error={
+                (errors && errors.zipcode_id) || zipcodeerror ? true : false
+              }
+              helperText={(errors && errors.zipcode_id) || zipcodeerror}
               InputProps={{
                 startAdornment: (
-                  <InputAdornment position="start">+63</InputAdornment>
+                  <InputAdornment position="start">
+                    {zipcodeloading && <CircularProgress size={15} />}
+                  </InputAdornment>
                 ),
               }}
-              value={staff.contact_num}
-              onChange={(ev) => {
-                const input = ev.target.value.slice(0, 10);
-                setStaff({ ...staff, contact_num: input });
-              }}
-              fullWidth
-              required
-              error={errors && errors.contact_num ? true : false}
-              helperText={errors && errors.contact_num}
             />
-            <TextField
-              id="Zone"
-              label="Zone/Block/Street"
-              size="small"
-              value={staff.zone}
-              onChange={(ev) => setStaff({ ...staff, zone: ev.target.value })}
-              fullWidth
-              required
-              error={errors && errors.zone ? true : false}
-              helperText={errors && errors.zone}
-            />
-            <TextField
-              id="Barangay"
-              label="Barangay"
-              size="small"
-              value={staff.barangay}
-              onChange={(ev) =>
-                setStaff({ ...staff, barangay: ev.target.value })
-              }
-              fullWidth
-              required
-              error={errors && errors.barangay ? true : false}
-              helperText={errors && errors.barangay}
-            />
-
-            <Box display={"flex"} flexDirection={"row"} sx={{ width: "100%" }}>
-              <TextField
-                id="Zipcode"
-                label="Zipcode"
-                size="small"
-                type="number"
-                value={selectedZipcode}
-                onChange={handleZipcodeChange}
-                fullWidth={!zipcode.area}
-                required
-                error={
-                  (errors && errors.zipcode_id) || zipcodeerror ? true : false
-                }
-                helperText={(errors && errors.zipcode_id) || zipcodeerror}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      {zipcodeloading && <CircularProgress size={15} />}
-                    </InputAdornment>
-                  ),
-                }}
-              />
-
+            <Box>
               <TextField
                 id="Area"
                 label="Area"
                 size="small"
                 value={zipcode.area || ""}
-                fullWidth
                 required
                 InputProps={{
                   readOnly: true,
                   "aria-readonly": true,
                 }}
-                error={errors && errors.zipcode_id ? true : false}
               />
+            </Box>
+            <Box>
               <TextField
                 id="Province"
                 label="Province"
@@ -387,74 +278,20 @@ export default function StaffForm() {
                   readOnly: true,
                   "aria-readonly": true,
                 }}
-                error={errors && errors.zipcode_id ? true : false}
               />
             </Box>
           </Box>
-        );
-      default:
-        return "Unknown step";
-    }
-  };
-
-  return (
-    <Paper
-      sx={{
-        width: "70%",
-        margin: "auto",
-        marginTop: "3%",
-        padding: "20px",
-        border: "2px solid black",
-      }}
-    >
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <div>
-        {activeStep === steps.length ? (
-          <div>
-            <p>All steps completed</p>
-          </div>
-        ) : (
-          <div>
-            <form
-              onSubmit={(e) => handleNext(e)}
-              style={{ alignItems: "center" }}
-            >
-              {getStepContent(activeStep)}
-              <Box
-                sx={{
-                  padding: "10px",
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <Button disabled={activeStep === 0} onClick={handlePrev}>
-                  Back
-                </Button>
-                {activeStep === 0 && (
-                  <>
-                    <Button variant="contained" color="primary" type="submit">
-                      Next
-                    </Button>
-                  </>
-                )}
-                {activeStep === 1 && (
-                  <>
-                    <Button variant="contained" color="primary" type="submit">
-                      Save
-                    </Button>
-                  </>
-                )}
-              </Box>
-            </form>
-          </div>
-        )}
-      </div>
+          <LoadingButton
+          loading={loading}
+          type="submit"
+          variant="contained"
+          disabled={zipcodeloading}
+          fullWidth
+        >
+          Save
+        </LoadingButton>
+        </Box>
+      </form>
     </Paper>
   );
 }
